a2413630e7bf31a7a358e3348e0ae80ad856ef9c
jojogreen <jojogreen1408@gmail.com>
2024-02-27 16:41:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StreamWriter sw = new StreamWriter("secret_plan.txt");
+
+        sw.WriteLine("How I'll defeat Captain Amazing");
+        sw.WriteLine("Another genius secret plan by Thw Swindler");
+        sw.WriteLine("I'll unleash my army of clines upon the citizens of Objectvill");
+
+        string location = "the mall";
+        for(int number = 1; number <= 5;  number++) //ternar
+        {
+            sw.WriteLine("Clone #{0} attacks {1}", number, location);
+            location = (location == "the mall") ? "downtown" : "the mall";
+        }
+        sw.Close();
+    }
+}
